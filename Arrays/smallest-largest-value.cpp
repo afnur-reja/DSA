7d79845e31dad9e7a4d8c315b66b480d21afd4fc
Afnur Reja <afnurreja884@gmail.com>
2025-07-21 12:57:04 +0530

    solve smallest and largest value in an array problem

--- Arrays/smallest-largest-value.cpp
+++ Arrays/smallest-largest-value.cpp
@@ -1,1 +1,22 @@
-// min(a, b) and max(a, b)
+#include <iostream>
+#include <climits> 
+using namespace std;
+
+int main() {
+    int nums[] = {5, 15, 9, 1, -15, 25};
+    int size = 6;
+    
+    int largest = INT_MIN;
+    int smallest = INT_MAX;
+
+    for (int i = 0; i < size; i++){
+        smallest = min(smallest, nums[i]);
+        largest = max(largest, nums[i]);
+    }
+
+    cout<<"Largest = "<<largest<<endl;
+    cout<<"Smallest = "<<smallest<<endl;
+
+
+    return 0;
+}
